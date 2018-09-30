6a6917674f665ecf8559c45edabcbce0d90ad357
Steve Thompson <writetome51@gmail.com>
2018-09-30 00:53:06 -0600

    added adjacentToValue() method

--- OpenArrayItemReplacer.js
+++ OpenArrayItemReplacer.js
@@ -13,10 +13,13 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
-var OpenArrayContainer_1 = require("./OpenArrayContainer");
-var replaceAt_replaceAdjacentAt_1 = require("intuitive-array-handlers/modify/return_void/replaceAt_replaceAdjacentAt");
-var replaceAllOf_replaceAllOfEach_1 = require("intuitive-array-handlers/modify/return_void/replaceAllOf_replaceAllOfEach");
-var replaceFirstOf_replaceFirstOfEach_1 = require("intuitive-array-handlers/modify/return_void/replaceFirstOf_replaceFirstOfEach");
+var errorIfNotFunction_1 = require("basic-data-handling/errorIfNotFunction");
+var OpenArrayContainer_1 = require("@writetome51/open-array-container/OpenArrayContainer");
+var replaceAt_replaceAdjacentAt_1 = require("@writetome51/array-insert-replace-basic/replaceAt_replaceAdjacentAt");
+var replaceMiddle_1 = require("@writetome51/array-insert-replace-basic/replaceMiddle");
+var replaceAllOf_replaceAllOfEach_1 = require("@writetome51/array-replace-intermediate/replaceAllOf_replaceAllOfEach");
+var replaceFirstOf_replaceFirstOfEach_1 = require("@writetome51/array-replace-intermediate/replaceFirstOf_replaceFirstOfEach");
+var replaceAdjacentToValue_1 = require("@writetome51/array-replace-intermediate/replaceAdjacentToValue");
 var OpenArrayItemReplacer = /** @class */ (function (_super) {
     __extends(OpenArrayItemReplacer, _super);
     function OpenArrayItemReplacer(data) {
@@ -28,37 +31,61 @@ var OpenArrayItemReplacer = /** @class */ (function (_super) {
     OpenArrayItemReplacer.prototype.at = function (index, newValue) {
         return this.returnThis_after(replaceAt_replaceAdjacentAt_1.replaceAt(index, newValue, this.data));
     };
-    // index can be negative or positive.
-    OpenArrayItemReplacer.prototype.adjacentAt = function (index, newValues) {
-        return this.returnThis_after(replaceAt_replaceAdjacentAt_1.replaceAdjacentAt(index, newValues, this.data));
+    // startingIndex can be negative or positive.
+    OpenArrayItemReplacer.prototype.adjacentAt = function (startingIndex, newValues) {
+        return this.returnThis_after(replaceAt_replaceAdjacentAt_1.replaceAdjacentAt(startingIndex, newValues, this.data));
     };
+    //  info = {
+    //		value: value to search for,
+    //		offset: position, relative to value, where replacing begins (can be negative or positive int),
+    //		howMany: number of items to replace 
+    //    }
+    //  Example:
+    //  array is [1,2,3,4,5,6,7,8] .
+    //  let newValues = [20,30,40];
+    //  this.adjacentToValue({value: 5, offset: -1, howMany: 2}, newValues);
+    //  array is now [1,2,3,20,30,40,6,7,8]
+    OpenArrayItemReplacer.prototype.adjacentToValue = function (info, newValues) {
+        return this.returnThis_after(replaceAdjacentToValue_1.replaceAdjacentToValue(info, newValues, this.data));
+    };
+    // You would use .middle() if the only parts of the array you want to leave
+    // alone are an equal number of items at the beginning and end.  Decide the
+    // numItemsToKeepAtEachEnd, and everything in-between will be replaced with newValues.
+    // Example: if array is [1,2,3,4,5,6,7] , and you call .middle(2, [9,10])
+    // the result will be [1,2,9,10,6,7] .  It preserves the first 2 items and the last 2.
+    OpenArrayItemReplacer.prototype.middle = function (numItemsToKeepAtEachEnd, newValues) {
+        return this.returnThis_after(replaceMiddle_1.replaceMiddle(numItemsToKeepAtEachEnd, newValues, this.data));
+    };
+    // Replaces first instance of value.
     OpenArrayItemReplacer.prototype.firstOf = function (value, newValue) {
         return this.returnThis_after(replaceFirstOf_replaceFirstOfEach_1.replaceFirstOf(value, newValue, this.data));
     };
     OpenArrayItemReplacer.prototype.firstOfEach = function (values, newValues) {
         return this.returnThis_after(replaceFirstOf_replaceFirstOfEach_1.replaceFirstOfEach(values, newValues, this.data));
     };
+    // Replaces all instances of value.
     OpenArrayItemReplacer.prototype.allOf = function (value, newValue) {
         return this.returnThis_after(replaceAllOf_replaceAllOfEach_1.replaceAllOf(value, newValue, this.data));
     };
+    // Replaces all instances of each value in values.
     OpenArrayItemReplacer.prototype.allOfEach = function (values, newValues) {
         return this.returnThis_after(replaceAllOf_replaceAllOfEach_1.replaceAllOfEach(values, newValues, this.data));
     };
-    OpenArrayItemReplacer.prototype.adjacentWithOne = function (newValue, startIndex, numItemsToReplace) {
-        // @ts-ignore
-        return this.returnThis_after(this.data.fill(newValue, startIndex, startIndex + numItemsToReplace));
+    // Loops thru array, passing each item into conversionFunction.
+    // conversionFunction signature:  function(itemValue, itemIndex, entireArray){...}
+    // conversionFunction must return the new value you want to give to that item.
+    OpenArrayItemReplacer.prototype.eachBy = function (conversionFunction) {
+        errorIfNotFunction_1.errorIfNotFunction(conversionFunction);
+        var index = -1;
+        while (++index < this.data.length) {
+            this.data[index] = conversionFunction(this.data[index], index, this.data);
+        }
+        return this;
     };
-    OpenArrayItemReplacer.prototype.allWithOne = function (value) {
+    // Replaces every single item with instance of newValue.
+    OpenArrayItemReplacer.prototype.allWithOne = function (newValue) {
         // @ts-ignore
-        return this.returnThis_after(this.data.fill(value));
-    };
-    // numBookendsToPreserve is how many items at the head and tail you don't want
-    // to replace.
-    // Example: if array is [1,2,3,4,5,6,7] , and you call .middle([9,10], 2)
-    // the result will be [1,2,9,10,6,7] .  It preserves the first 2 items and
-    // the last 2.
-    OpenArrayItemReplacer.prototype.middle = function (values, numBookendsToPreserve) {
-        return this.returnThis_after(replaceMiddle(values, numBookendsToPreserve, this.data));
+        return this.returnThis_after(this.data.fill(newValue));
     };
     return OpenArrayItemReplacer;
 }(OpenArrayContainer_1.OpenArrayContainer));
--- OpenArrayItemReplacer.ts
+++ OpenArrayItemReplacer.ts
@@ -1,10 +1,15 @@
-import { OpenArrayContainer } from './OpenArrayContainer';
+import { errorIfNotFunction } from 'basic-data-handling/errorIfNotFunction';
+import { IAdjacentToValueInfo }
+	from '@writetome51/adjacent-to-value-info-interface/IAdjacentToValueInfo';
+import { OpenArrayContainer } from '@writetome51/open-array-container/OpenArrayContainer';
 import { replaceAdjacentAt, replaceAt }
 	from '@writetome51/array-insert-replace-basic/replaceAt_replaceAdjacentAt';
+import { replaceMiddle } from '@writetome51/array-insert-replace-basic/replaceMiddle';
 import { replaceAllOf, replaceAllOfEach }
 	from '@writetome51/array-replace-intermediate/replaceAllOf_replaceAllOfEach';
 import { replaceFirstOf, replaceFirstOfEach }
 	from '@writetome51/array-replace-intermediate/replaceFirstOf_replaceFirstOfEach';
+import { replaceAdjacentToValue } from '@writetome51/array-replace-intermediate/replaceAdjacentToValue';
 
 
 export class OpenArrayItemReplacer extends OpenArrayContainer {
@@ -19,55 +24,91 @@ export class OpenArrayItemReplacer extends OpenArrayContainer {
 
 
 	// index can be negative or positive.
+
 	at(index, newValue): this {
 		return this.returnThis_after(replaceAt(index, newValue, this.data));
 	}
 
 
-	// index can be negative or positive.
-	adjacentAt(index, newValues: any[]): this {
-		return this.returnThis_after(replaceAdjacentAt(index, newValues, this.data));
+	// startingIndex can be negative or positive.
+
+	adjacentAt(startingIndex, newValues: any[]): this {
+		return this.returnThis_after(replaceAdjacentAt(startingIndex, newValues, this.data));
 	}
 
+
+	//  info = {
+	//		value: value to search for,
+	//		offset: position, relative to value, where replacing begins (can be negative or positive int),
+	//		howMany: number of items to replace 
+	//    }
+	//  Example:
+	//  array is [1,2,3,4,5,6,7,8] .
+	//  let newValues = [20,30,40];
+	//  this.adjacentToValue({value: 5, offset: -1, howMany: 2}, newValues);
+	//  array is now [1,2,3,20,30,40,6,7,8]
+
+	adjacentToValue(info: IAdjacentToValueInfo, newValues: any[]): this {
+		return this.returnThis_after(replaceAdjacentToValue(info, newValues, this.data));
+	}
+
+
+	// You would use .middle() if the only parts of the array you want to leave
+	// alone are an equal number of items at the beginning and end.  Decide the
+	// numItemsToKeepAtEachEnd, and everything in-between will be replaced with newValues.
+	// Example: if array is [1,2,3,4,5,6,7] , and you call .middle(2, [9,10])
+	// the result will be [1,2,9,10,6,7] .  It preserves the first 2 items and the last 2.
+
+	middle(numItemsToKeepAtEachEnd, newValues: any[]) {
+		return this.returnThis_after(replaceMiddle(numItemsToKeepAtEachEnd, newValues, this.data));
+	}
+
+
+	// Replaces first instance of value.
 
 	firstOf(value, newValue): this {
 		return this.returnThis_after(replaceFirstOf(value, newValue, this.data));
 	}
 
 
-	firstOfEach(values, newValues): this {
+	firstOfEach(values: any[], newValues: any[]): this {
 		return this.returnThis_after(replaceFirstOfEach(values, newValues, this.data));
 	}
 
+
+	// Replaces all instances of value.
 
 	allOf(value, newValue): this {
 		return this.returnThis_after(replaceAllOf(value, newValue, this.data));
 	}
 
 
-	allOfEach(values, newValues): this {
+	// Replaces all instances of each value in values.
+
+	allOfEach(values: any[], newValues: any[]): this {
 		return this.returnThis_after(replaceAllOfEach(values, newValues, this.data));
 	}
 
 
-	adjacentWithOne(startIndex, newValue, numItemsToReplace): this {
-		// @ts-ignore
-		return this.returnThis_after(this.data.fill(newValue, startIndex, startIndex + numItemsToReplace));
+	// Loops thru array, passing each item into conversionFunction.
+	// conversionFunction signature:  function(itemValue, itemIndex, entireArray){...}
+	// conversionFunction must return the new value you want to give to that item.
+
+	eachBy(conversionFunction: Function): this {
+		errorIfNotFunction(conversionFunction);
+		let index = -1;
+		while (++index < this.data.length) {
+			this.data[index] = conversionFunction(this.data[index], index, this.data);
+		}
+		return this;
 	}
 
 
-	allWithOne(value: any): this {
+	// Replaces every single item with instance of newValue.
+
+	allWithOne(newValue): this {
 		// @ts-ignore
-		return this.returnThis_after(this.data.fill(value));
-	}
-
-
-	// numItemsToPreserveAtEachEnd is how many items at the head and tail you don't want
-	// to replace.
-	// Example: if array is [1,2,3,4,5,6,7] , and you call .middle(2, [9,10])
-	// the result will be [1,2,9,10,6,7] .  It preserves the first 2 items and the last 2.
-	middle(numItemsToPreserveAtEachEnd, values){
-		return this.returnThis_after(replaceMiddle(numItemsToPreserveAtEachEnd, values, this.data));
+		return this.returnThis_after(this.data.fill(newValue));
 	}
 
 
