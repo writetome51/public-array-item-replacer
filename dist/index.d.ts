75c1b8f529368ac58d74f86684736710670969f7
Steve Thompson <writetome51@gmail.com>
2018-11-21 21:36:27 -0700

    minor change

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,10 +3,17 @@ import { PublicArrayContainer } from '@writetome51/public-array-container';
 
 
 export declare class PublicArrayReplacer extends PublicArrayContainer {
+
 	constructor(data?: any[]);
 
 
+	at(index: any, newValue: any): this;
+
+
 	adjacentAt(startingIndex: any, newValues: any[]): this;
+
+
+	adjacentVariableAt(startingIndex: any, howManyToReplace: number, newValues: any): this;
 
 
 	adjacentToValue(info: IAdjacentToValueInfo, newValues: any[]): this;
@@ -16,11 +23,11 @@ export declare class PublicArrayReplacer extends PublicArrayContainer {
 	 Only applies to the first instance of value found in array.
 	 The parameter 'info' is an object that looks like this:
 	 {
-                    value: any except object (the value to search for in the array),
-                    offset: integer (tells function where, in relation to value, to begin selecting adjacent
+        value: any except object (the value to search for in the array),
+        offset: integer (tells function where, in relation to value, to begin selecting adjacent
                             items to replace.  If offset is zero, the selection will begin with value.)
-                    howMany: integer greater than zero (it's how many adjacent items to replace)
-             }
+        howMany: integer greater than zero (it's how many adjacent items to replace)
+     }
 
 	 Example:
 	 //  array is [1,2,3,4,5,6,7,8] .
@@ -46,7 +53,7 @@ export declare class PublicArrayReplacer extends PublicArrayContainer {
 	allOfEach(values: any[], newValues: any[]): this;
 
 
-	eachBy(conversionFunction: (item: any, index?: any, array?: any) => any): this;
+	each(replacementFunction: (item: any, index?: any, array?: any) => any): this;
 
 
 	allWithOne(values: any[], newValue: any): this;
